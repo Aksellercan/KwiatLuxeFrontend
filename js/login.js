4e6bec78d6f809c413e8def2f7da2cb01c8166a2
Aksell <akselercan2003@gmail.com>
2025-06-12 17:05:01 +0200

    refactor of login and connection

--- js/login.js
+++ js/login.js
@@ -1,51 +1,30 @@
+var apiBaseUrl = "https://localhost:7272";
+const messageDiv = document.getElementById("message");
+
 document.getElementById("loginForm").addEventListener("submit", async function(e) {
   e.preventDefault();
 
   const usernameInput = document.getElementById("username").value.trim();
   const passwordInput = document.getElementById("password").value.trim();
-  const messageDiv = document.getElementById("message");
-
-  // admin control 
-  if (usernameInput === "admin" && passwordInput === "admin") {
-    localStorage.setItem("username", "admin");
-    localStorage.setItem("role", "admin");
-    window.location.href = "../pages/admin.html";  
-    return;  // important
-  }
-
-  // Test user control
-  if (usernameInput === "testuser" && passwordInput === "1234") {
-    localStorage.setItem("username", "testuser");
-    localStorage.setItem("role", "user");
-    window.location.href = "../pages/user.html";  
-    return;  
-  }
-
+  const emailInput = document.getElementById("email").value.trim();
   
-  try {
-    const response = await fetch("https://api.example.com/login", {
-      method: "POST",
-      headers: { "Content-Type": "application/json" },
-      body: JSON.stringify({ username: usernameInput, password: passwordInput }),
-    });
-
-    const data = await response.json();
-
-    if (response.ok) {
-      localStorage.setItem("username", data.username);
-      localStorage.setItem("role", data.role);
-
-      if (data.role === "admin") {
-        window.location.href = "../pages/admin.html";
-      } else {
-        window.location.href = "../pages/user.html";
-      }
-    } else {
-      messageDiv.textContent = data.message || "Login failed";
-      messageDiv.style.color = "red";
-    }
-  } catch (error) {
-    messageDiv.textContent = "Network error, try again later.";
-    messageDiv.style.color = "red";
-  }
+  await requestLogin(usernameInput, passwordInput, emailInput);
 });
+
+async function requestLogin(usernameInput, passwordInput, emailInput) {
+  const response = await fetch(apiBaseUrl + "/Auth/login", {
+      method: "POST",
+      headers: {"Content-Type": "application/json"},
+      body: JSON.stringify({ username: usernameInput, password: passwordInput, email: emailInput }),
+    });
+    if (response.status != 200) {
+      messageDiv.textContent = "Login failed";
+      messageDiv.style.color = "red";
+    } else {
+      const getToken = await response.json();
+      localStorage.setItem('token', getToken.token);
+      window.location.href = '../pages/user.html';
+      return;
+    }
+    return;
+}
